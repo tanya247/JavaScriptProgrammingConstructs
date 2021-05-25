274223385cfc07a57b19351b30e809a5d8e0382e
user name <user email>
2021-05-25 13:19:30 +0530

    Addition: Adding IF_ELSE_IF to get digit word

---
+++ UnitDigit.js
@@ -0,0 +1,8 @@
+function Digit(num){
+    if (num == 1) console.log("Ones");
+    else if (num == 10) console.log("Ten");
+    else if (num == 100) console.log("Hundered");
+    else if (num == 1000) console.log("Thousand");
+    else if (num == 10000) console.log("Ten Thousand");
+}
+Digit(100);
