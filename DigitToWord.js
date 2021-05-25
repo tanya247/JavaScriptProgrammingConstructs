535e02bda0611ace254a2a9c5b22d68274582763
user name <user email>
2021-05-25 13:16:56 +0530

    Addition: Adding IF_ELSE_IF to get digit in words

---
+++ DigitToWord.js
@@ -0,0 +1,22 @@
+function DigitToWord(digit){
+    if(digit == 1){
+        console.log(digit+" : One");
+    }else if(digit == 2){
+        console.log(digit+" : Two");
+    }else if(digit == 3){
+        console.log(digit+" : Three");
+    }else if(digit == 4){
+        console.log(digit+" : Four");
+    }else if(digit == 5){
+        console.log(digit+" : Five");
+    }else if(digit == 6){
+        console.log(digit+" : Six");
+    }else if(digit == 7){
+        console.log(digit+" : Seven");
+    }else if(digit == 8){
+        console.log(digit+" : Eight");
+    }else if(digit == 9){
+        console.log(digit+" : Nine");
+    }
+}
+DigitToWord(6);
