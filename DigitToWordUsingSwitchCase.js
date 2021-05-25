32619f4e8d3227a2647cba7f0216ca5a036625c5
user name <user email>
2021-05-25 13:27:06 +0530

    Addition: Adding Switch case for converting digit to word

---
+++ DigitToWordUsingSwitchCase.js
@@ -0,0 +1,25 @@
+function DigitToWordUsingSwitchCase(digit){
+    switch(digit){
+        case 1: console.log(digit+" : One");
+        break;
+        case 2: console.log(digit+" : Two");
+        break;
+        case 3: console.log(digit+" : Three");
+        break;
+        case 4: console.log(digit+" : Four");
+        break;
+        case 5: console.log(digit+" : Five");
+        break;
+        case 6: console.log(digit+" : Six");
+        break;
+        case 7: console.log(digit+" : Seven");
+        break;
+        case 8: console.log(digit+" : Eight");
+        break;
+        case 9: console.log(digit+" : Nine");
+        break;
+        default: console.log("Invalid");
+    }    
+    
+}
+DigitToWordUsingSwitchCase(4);
